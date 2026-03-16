0ad6d0138b2cdb8945b09e98f4dd17543d862fbf
Aidan Hibbard <aidanchibbard@gmail.com>
2026-03-15 22:06:28 -0700

    more ci

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,3 +1,9 @@
 import antfu from '@antfu/eslint-config'
 
-export default antfu()
+export default antfu({
+  typescript: {
+    overrides: {
+      '@typescript-eslint/no-explicit-any': 'error',
+    },
+  },
+})
